5cddc54d23f8bebe1f364caa204a2221072fc4cd
james3126 <jk3126@hotmail.co.uk>
2018-07-03 03:37:40 +0100

    Full re-write to handle pip versions <9 and >10

--- EzReqInstaller.py
+++ EzReqInstaller.py
@@ -1,39 +1,47 @@
-# Requirements Installer v1.0ddns - James Kerley 2018
-requiredPackages = ['requests','time','os','sys','json']
-packageInstallNameDict = {'requests': 'requests','time': False,'os': False,'sys': False,'json': False}
-missingPackages = []
+# Requirements Installer v1.1ddns - James Kerley 2018
+import sys
 
 def install(package):
-    pip.main(['install', package])
+    main(['install', package])
 
-for package in requiredPackages:
-    try:
-        globals()[package] = __import__(package)
-    except ImportError:
-        missingPackages.append(package)
-        print("Failed to import %s. Will try to install..." % str(package))
-    else:
-        print("Imported %s" % str(package))
-
-if len(missingPackages) > 0:
-    print("\n\nAttempting to install missing packages...")
+def is_new_pip():
     try:
         import pip
     except ImportError:
-        print("Failed...")
-        sys.exit(0)
-        
+        print("Failed. Unknown error. Exiting...")
+        exit()
+
+    PIP_VERSION = pip.__version__
+
+    if int(PIP_VERSION.split('.')[0]) >= 10:
+        return True
+    else:
+        return False
+
+def is_installed(package, attempt=1):
+    try:
+        globals()[package] = __import__(package)
+    except ImportError:
+        if attempt == 1:
+            missingPackages.append(package)
+            print("Failed to import %s. Will try to install..." % str(package))
+        else:
+            print("Unknown error at this time. Please contact me for help! Exiting...")
+            exit()
+    else:
+        print("Imported %s" % str(package))
+
+for package in requiredPackages:
+    is_installed(package)
+
+if len(missingPackages) > 0:
+    print("\n\nAttempting to install missing packages...")
+
+    if is_new_pip():
+        from pip._internal import main
+
     for package in missingPackages:
-        packageInstallName = packageInstallNameDict[package]
         print("Preparing for %s installation...\n" % str(packageInstallName))
         install(packageInstallName)
 
-        try:
-            globals()[package] = __import__(package)
-        except ImportError:
-            print("Unknown error at this time. Please contact me for help! Exiting...")
-            exit()
-        else:
-            print("Imported %s" % str(package))
-
-        print("All requirements instlled")
+        is_installed(package, 2)
