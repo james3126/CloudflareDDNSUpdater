bfd21700bbd849d3cc4a8c9b9180d22cde5549c8
james3126 <jk3126@hotmail.co.uk>
2018-09-18 10:08:51 +0100

    Bug fixes and improper syntax fix.

--- req.py
+++ req.py
@@ -3,14 +3,14 @@ import urllib.request
 import json
 
 class request:
-    def get_request(url, headers=None, json=False):
+    def get_request(url, headers=None, jsonOut=False):
         if headers != None:
-            req = urllib.request.Request(url)
+            req = urllib.request.Request(url, headers=headers)
         else:
-            req = urllib.request.Request(url, headers)
+            req = urllib.request.Request(url)
         raw_response = urllib.request.urlopen(req)
-        reponse = raw_response.read().decode('utf-8')
-        if json:
+        response = raw_response.read().decode('utf-8')
+        if jsonOut:
             json_obj = json.loads(response)
             return json_obj
         else:
