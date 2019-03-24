2f10a0d36e0846dbc563febcb6b63094348a9045
james3126 <jk3126@hotmail.co.uk>
2019-03-24 17:15:06 +0000

    Handling for HTTPError as a result of auth error
    Customized the reqs code to be a little more task specific, and handle 400 errors that will be thrown from rejected authentication. Previously causing crashes

--- rebuild/reqs.py
+++ rebuild/reqs.py
@@ -1,4 +1,4 @@
-# HTTP GET/PUT module without 'requests' - James Kerley 2018
+# HTTP GET/PUT module without 'requests' - James Kerley 2019
 import urllib.request
 import json
 
@@ -8,7 +8,13 @@ class request:
             req = urllib.request.Request(url, headers=headers)
         else:
             req = urllib.request.Request(url)
-        raw_response = urllib.request.urlopen(req)
+
+        try:
+            raw_response = urllib.request.urlopen(req)
+        except urllib.request.HTTPError as e:
+            print("ERROR: {}\nERROR: It is likely your details are inccorect! Please check and try again!".format(e))
+            exit()
+
         response = raw_response.read().decode('utf-8')
         if jsonOut:
             json_obj = json.loads(response)
