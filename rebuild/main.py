2e4efb8e37203bb8071f04f0a4e1203b7e99b2b1
james3126 <jk3126@hotmail.co.uk>
2019-03-24 17:17:17 +0000

    Created successful run-through

--- rebuild/main.py
+++ rebuild/main.py
@@ -6,6 +6,8 @@ import sys
 
 debugComment = lambda x: print(f'DEBUG: {x}') 
 
+DEBUG=True
+
 def is_windows():
     # debugComment('finding out OS...')
 
@@ -27,3 +29,23 @@ RecordUpdater = CDNSU(apiKey,email,webAddress,is_windows())
 #    print('Please ensure you are connected to the internet, and have access to CloudFlare\'s servers')
 #    sys.exit(0)
 print(f'Online: {RecordUpdater.isOnline()}')
+
+print(RecordUpdater.getIp())
+print(RecordUpdater.getZoneId())
+print(RecordUpdater.getIdOldipPrxstate())
+
+firstRun=True
+
+if RecordUpdater.isOnline():
+    if not firstRun:
+        firstRun = False
+        RecordUpdater.oldIp = RecordUpdater.currentIp
+    RecordUpdater.getIp()
+
+    if RecordUpdater.chkForChanges():
+        RecordUpdater.updateRecord()
+
+else:
+    print("Not online currently! Waiting for reconnection...")
+
+sys.exit(0)
